b20d7ebdd6b0ed371bc7d5d9c64fc7af2a7ed796
AndrusGerman <andrusdiazaleman@gmail.com>
2022-06-04 11:43:05 -0400

    print the ip in server mode

--- go.mod
+++ go.mod
@@ -2,3 +2,4 @@ module github.com/AndrusGerman/remotepipe
 
 go 1.18
 
+require github.com/AndrusGerman/getmyip v0.0.0-20220604153815-ae32bca3b0e9 // indirect
--- server/execute_server.go
+++ server/execute_server.go
@@ -1,9 +1,14 @@
 package server
 
-import "log"
+import (
+	"log"
+
+	"github.com/AndrusGerman/getmyip"
+)
 
 func Execute() {
-	log.Println("server is start")
+	log.Println("server: is start")
+	log.Println("server: ip ", getmyip.GetLocalIP())
 	var err = start_dial_tcp_server()
 	if err != nil {
 		log.Println("server: err start server ", err)
